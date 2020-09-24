0020b608abb23d3d13cf0a2da7aad41daf526b96
Erin Claudio <eclaudio@gmail.com>
2020-09-24 11:08:03 -0400

    Statistics Class added and some refactoring

--- test/Gradebook.Tests/BookTests.cs
+++ test/Gradebook.Tests/BookTests.cs
@@ -5,10 +5,22 @@ namespace Gradebook.Tests
 {
     public class BookTests
     {
-        [Fact]
+        [Fact]        
         public void Test1()
         {
+            // arrange
             var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+            
+            // act
+            var result = book.GetStatistics();
+            
+            // assert
+            Assert.Equal(85.6, result.Average);
+            Assert.Equal(90.5, result.high);
+            Assert.Equal(77.3, result.low);
         }
     }
 }
